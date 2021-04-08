5669e8afbdb3edf13e5fb769f26a2fecd88189c3
Akaruip <paulovsilva3@outlook.com>
2021-04-08 17:39:35 -0300

    first commit

---
+++ looking_up_giphs/looking_up_giphs/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:looking_up_giphs/ui/homepage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+
+void main() {
+
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarBrightness: Brightness.light,
+      statusBarColor: Colors.transparent));
+  runApp(MaterialApp(
+    home: HomePage(),
+        debugShowCheckedModeBanner: false,));
+}
+
+
