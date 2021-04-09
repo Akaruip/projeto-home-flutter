18b48cbf6612c55351f43e59a42134c389971b8b
Akaruip <paulovsilva3@outlook.com>
2021-04-09 14:45:39 -0300

    try another thing

---
+++ agenda_contatos/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:url_launcher/url_launcher.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyHomePage()
+  ));
+}
+
+class MyHomePage extends StatefulWidget {
+   @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+
+    );
+  }
+}
